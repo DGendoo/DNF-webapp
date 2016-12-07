6a4b0f79d3d569d265aacbb49dc5b3877f520329
chuntul <chantal.ho98@hotmail.com>
2016-12-06 22:49:31 -0500

    Revert "Server requests for score json files"
    This reverts commit 80c680b7ef594c0a150581ff8b5eca51bfd6f34d.

--- src/app/server/api/thing/index.js
+++ src/app/server/api/thing/index.js
@@ -10,7 +10,5 @@ router.get('/drug_list/:id', controller.drugList);
 router.get('/drug_network/:id', controller.drugNetwork);
 router.get('/drug_clusters/:id', controller.drugClusters);
 router.get('/exemplar/:id', controller.exemplar);
-router.get('/scores/:id', controller.scores);
-
 
 module.exports = router;
